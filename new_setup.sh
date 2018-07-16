439bb806ec25f44ba9bec1890164fbdff60f88a0
x3830s <x3830@protonmail.com>
2018-07-16 18:06:10 -0400

    Folder, file and user check

--- new_setup.sh
+++ new_setup.sh
@@ -9,36 +9,71 @@ powerline_fonts="https://www.github.com/powerline/fonts"
 linux_environment_dir="Linux-Environment"
 powerline_fonts_dir="fonts"
 
+#TODO: Check if user is root or not (generally for Kali)
+if [ "$(whoami)" = "root" ]
+then
+	dir='/root'
+else
+	dir="$HOME"
+fi 
+
+
 # Check and download Linux-Environment repo
 github_repo () {
-	# Don't ask for pass if can't connect or wrong repo
-	`GIT_ASKPASS=true git ls-remote "$1" > /dev/null 2>&1`
-	if [ "$?" -ne 0 ] 
+	# TODO: Create a github folder with checks to see if it exists
+	if [ ! -d "$dir/github" ]
 	then
-		touch install.log
-		echo -e "[Failed] $1.\n" >> install.log
-		echo "Unable to connect to $1. Check install.log"
-		exit 1
-	else # on success
-	 	git clone $1
-		if [ -d $2 ]	
+		echo "$0: You do not have a '$dir/github' folder, making one ..." >&2
+		mkdir -p "$dir/github"
+	elif [ -d "$dir/github/$2" ]
+	then
+		echo "[ ] $1 Already exists at '$dir/github/$2'" >> install.log
+	
+	else	
+		# Don't ask for pass if can't connect or wrong repo
+		`GIT_ASKPASS=true git ls-remote "$1" > /dev/null 2>&1`
+		if [ "$?" -ne 0 ] 
 		then
-			echo -e "\n[+] The package at $1 was successful, located in $2 \n"
-			echo -e "\n[+] The package at $1 was successful, located in $2 \n" >> install.log
+			touch install.log
+			echo -e "[Failed] $1.\n" >> install.log
+			echo "Unable to connect to $1. Check install.log"
+		else # on success
+			if [ ! -d "$dir/github/$2" ]
+			then
+				git clone $1 $dir/github/$2
+			else
+				echo -e "\n[ ] The package at $1 is already installed, and located at '$dir/github/$2'"
+				echo -e "\n[ ] The package at $1 is already installed, and located at '$dir/github/$2'" >> install.log
+
+				echo -e "\n[+] The package at $1 was successful, located in $2 \n"
+				echo -e "\n[+] The package at $1 was successful, located in $2 \n" >> install.log
+			fi
 		fi
 	fi
 }
 
+	
+# TODO: Move Auto-Setup to the github folder
+
 # TODO: Functions to create the working autoscript
+# # # # TODO: If folder exists do not redownload
 github_repo $linux_environment $linux_environment_dir
 github_repo $powerline_fonts $powerline_fonts_dir
 
 # TODO: Install Powerline fonts
-./fonts/install.sh
+# # # # TODO: If exists do not run install script 
+# # # # TODO: Place correct path to github folder
+if [ ! -e "$dir/.local/share/fonts/Meslo LG S Regular for Powerline" ]
+then
+	echo "Powerline Fonts already installed"	
+else
+	$dir/github/$powerline_fonts_dir/install.sh
+	echo -e "\n[+] Installed Powerline Fonts\n" >> install.log
+fi
 
-echo -e "\n[+] Moved .vimrc to ~.\n" >> install.log
 # TODO: Move .vimrc to appropriate folder
-mv "$linux_environment_dir/.vimrc" ./home
+cp "$dir/github/$linux_environment_dir/.vimrc" $dir 
+echo -e "\n[+] Moved .vimrc to ~.\n"
 echo -e "\n[+] Moved .vimrc to ~.\n" >> install.log
 
 # TODO: Move .xresources to appropriate folder
